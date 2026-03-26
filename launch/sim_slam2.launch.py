d6315e57bcb1ca00457e3e8fc88f5f034dcaf70c
Sean MacKenzie <sean_mackenzie@ucsb.edu>
2026-03-26 12:14:34 -0500

    added fully enclosed world (revised living_room.sdf) and revised slam launch files to get everything working.

--- launch/sim_slam2.launch.py
+++ launch/sim_slam2.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
@@ -18,20 +18,15 @@ def generate_launch_description():
 
     slam_params = os.path.join(pkg_path, 'config', 'slam_toolbox.yaml')
 
-    slam = TimerAction(
-        period=5.0,
-        actions=[
-            Node(
-                package='slam_toolbox',
-                executable='async_slam_toolbox_node',
-                name='slam_toolbox',
-                output='screen',
-                parameters=[
-                    slam_params,
-                    {'use_sim_time': True},
-                ],
-            )
-        ]
+    slam = Node(
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[
+            slam_params,
+            {'use_sim_time': True},
+        ],
     )
 
     return LaunchDescription([
