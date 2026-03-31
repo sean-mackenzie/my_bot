0cbbc9af68e0d893a3027ea4cbef7f0e31cb0476
Sean MacKenzie <sean_mackenzie@ucsb.edu>
2026-03-31 13:55:16 -0500

    got real_drive_slam.launch.py working

--- launch/real_drive_slam.launch.py
+++ launch/real_drive_slam.launch.py
@@ -5,7 +5,6 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -26,17 +25,18 @@ def generate_launch_description():
         }.items(),
     )
 
-    slam_params = os.path.join(pkg_path, 'config', 'slam_toolbox.yaml')
-
-    slam = Node(
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen',
-        parameters=[
-            slam_params,
-            {'use_sim_time': False},
-        ],
+    slam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('slam_toolbox'),
+                'launch',
+                'online_async_launch.py'
+            )
+        ),
+        launch_arguments={
+            'use_sim_time': 'false',
+            'slam_params_file': os.path.join(pkg_path, 'config', 'slam_toolbox.yaml'),
+        }.items(),
     )
 
     return LaunchDescription([
